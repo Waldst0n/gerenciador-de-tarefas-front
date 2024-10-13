dece7717c3e91aaeb005e4c13ef76d4971adc70b
Waldston <waldston.ws@gmail.com>
2024-10-13 12:10:08 -0300

    feat add fetch from backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import './App.css'
-import { useState } from 'react'
-import TaskItem from './components/TaskItem'
+import './App.css';
+import { useState, useEffect } from 'react';
+
+import TaskItem from './components/TaskItem';
+import axios from 'axios';
 
 const App = () => {
     const [tasks, setTasks] = useState([
@@ -14,14 +16,28 @@ const App = () => {
             description: 'Ler',
             isCompleted: true,
         },
-    ])
+    ]);
+
+    const fetchTasks = async (url) => {
+        try {
+            const { data } = await axios.get('http://localhost:8000/tasks');
+            setTasks(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
     return (
         <>
             {tasks.map((task) => (
                 <TaskItem key={task.id} task={task} />
             ))}
         </>
-    )
-}
+    );
+};
 
-export default App
+export default App;
