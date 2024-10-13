acbc59eab4dfea2179a629809a090e6d2e03e962
Waldston <waldston.ws@gmail.com>
2024-10-13 13:20:25 -0300

    feat: add TaskItem Component

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+import './tasks.scss';
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState([]);
+
+    const fetchTasks = async (url) => {
+        try {
+            const { data } = await axios.get('http://localhost:8000/tasks');
+            setTasks(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    return (
+        <div className="tasks-container">
+            <h2>Minhas tarefas</h2>
+
+            <div className="last-tasks">
+                <h3>Últimas tarefas</h3>
+                <div className="tasks-list">
+                    {tasks
+                        .filter((task) => task.isCompleted === false)
+                        .map((lastTask) => (
+                            <p>{lastTask.description}</p>
+                        ))}
+                </div>
+            </div>
+
+            <div className="completed-tasks">
+                <h3>Tarefas Concluídas</h3>
+                <div className="tasks-list">
+                    {tasks
+                        .filter((task) => task.isCompleted)
+                        .map((lastTask) => (
+                            <p>{lastTask.description}</p>
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Tasks;
