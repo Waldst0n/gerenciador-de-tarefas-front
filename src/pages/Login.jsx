f7dc1cdf2ee6678d56e2011378a2035372617bcb
Waldston <waldston.ws@gmail.com>
2024-10-15 19:38:17 -0300

    feat: add functionality on button to navigate on Home page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import logo from '../assets/img/Logotipo_empresa_de_programação_sistemas_e_tecnologia-removebg-preview.png';
+
+import { useNavigate } from 'react-router-dom';
 
 import Button from '../components/Button';
 
 import './Login.scss';
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const handleSignInClick = () => {
+        navigate('/');
+    };
+
     return (
         <div className="login-container">
-            <h1>Gerencia</h1>
+            <img src={logo} alt="Logo Waldston Santana" />
 
             <div className="button-container">
-                <Button>Entrar</Button>
+                <Button onClick={handleSignInClick}>Entrar</Button>
             </div>
         </div>
     );
